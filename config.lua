ad30404dd35fdc6f70e0729d55748e5f464087a7
yroxgaming <81095431+yroxgaming@users.noreply.github.com>
2022-12-22 01:19:01 +0100

    Delete config.lua

--- config.lua
+++
@@ -1,5 +0,0 @@
-Config = {}
-
-Config.Notify = 'esx' -- mythic (new) / esx
-Config.Webhook = 'your_webhook_here' -- Place your discord webhook here for using screenshot feature 
-Config.KeyMapping = 'delete' -- Control action for open the MDT
